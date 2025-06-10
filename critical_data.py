da3fea21ae1ae34bf273625b8faa2f4d8404402c
baldwin <baldwinmalabanan.bm@gmail.com>
2025-06-09 21:31:17 -0400

    add critical_data

---
+++ critical_data.py
@@ -0,0 +1,9 @@
+import os
+
+api_key = os.getenv("STM_API_KEY")
+api_url = os.getenv("STM_API_URL")
+
+vehicle_api_endpoint = os.getenv("VEHICLE_API_ENDPOINT")
+route_api_endpoint_template = os.getenv("ROUTE_API_ENDPOINT_TEMPLATE")
+
+
